bdac0b8576f2fc528fa0a2d2743140b81b9675de
Shannon Echegaray <shannonechegaray@gmail.com>
2023-12-01 23:18:07 -0300

    feat: integrating chakra-ui

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,6 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
+import { withEmotionCache } from "@emotion/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
@@ -9,25 +11,69 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import { ServerStyleContext, ClientStyleContext } from "./context";
+import { useContext, useEffect } from "react";
+
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
+interface DocumentProps {
+  children: React.ReactNode
+}
+
+const Document = withEmotionCache(
+  ({ children }: DocumentProps, emotionCache) => {
+    const serverStyleData = useContext(ServerStyleContext);
+    const clientStyleData = useContext(ClientStyleContext);
+
+    useEffect(() => {
+      emotionCache.sheet.container = document.head;
+      
+      const tags = emotionCache.sheet.tags;
+      emotionCache.sheet.flush();
+      tags.forEach((tag) => {
+        (emotionCache.sheet as any)._insertTag(tag);
+      });
+
+      clientStyleData?.reset();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <Meta />
+          <Links />
+          {
+            serverStyleData?.map(({ key, ids, css }) => (
+              <style
+                key={key}
+                data-emotion={`${key} ${ids.join(" ")}`}
+                dangerouslySetInnerHTML={{ __html: css }}
+              />
+            ))
+          }
+        </head>
+        <body>
+          { children }
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
+    );
+  }
+);
+
 export default function App() {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
+    <Document>
+      <ChakraProvider>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
+      </ChakraProvider>
+    </Document>
   );
 }
